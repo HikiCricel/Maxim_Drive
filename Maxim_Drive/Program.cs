caed84ad90cee0acb8cee84d1a2e0662e136c687
Hiki Cricel <daniilfisenko51005@gmail.com>
2025-11-21 13:44:37 +0300

    feat: create map - add map class

--- Maxim_Drive/Program.cs
+++ Maxim_Drive/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Maxim_Drive.Map;
+using System;
+using System.Threading.Channels;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter map sizes:");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        var map_b = new DefaultMap(x,y);
+        string[,] map = map_b.BuildMap();
+        Console.WriteLine("Result:");
+        for(int i = 0; i<map_b.Y; i++)
+        {
+            Console.Write('|');
+            for(int j = 0; j<map_b.X; j++)
+            {
+                Console.Write($"{map[i,j], 3}");
+            }
+            Console.Write('|');
+            Console.WriteLine();
+        }
+    }
+}
